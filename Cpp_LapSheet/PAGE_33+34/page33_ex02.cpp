8f8ded7624cccace8f3948376e13f08a6bd7da8d
Omar Al-mashaqbeh <o.sh.almashaqba@gmail.com>
2025-12-29 20:32:05 +0300

    Edit

---
+++ Cpp_LapSheet/PAGE_33+34/page33_ex02.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+void fillSalaries (double salaries[], int size)
+{
+    for (int i=0; i<10; i++)
+    {
+        /* code */
+    }
+    
+}
+double getMaximum (double salaries[], int size)
+{
+
+}
+double getAverage (double salaries[], int size)
+{
+
+}
+char getGender (double salaries [] ,char gender[], int size)
+{
+
+}
+char getGenderForMin (double salaries [] ,char gender[], int size)
+{
+
+}
+int main()
+{
+    char gender[10];
+    double salaries[10];
+    for (int i=0; i<10; i++)
+    {
+        cout<<"Enter 'M' if the gender is male OR 'F' if the gender is female"<<endl;
+        cin>>gender[i];
+    }
+    
+    return 0;
+}
