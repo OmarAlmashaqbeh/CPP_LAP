6b5bd6bbc050cc2c0e9ca5513c54142d8287f077
Omar Al-mashaqbeh <o.sh.almashaqba@gmail.com>
2026-05-19 20:50:42 +0300

    FIX

--- Cpp_LabSheet/PAGE_02/page02_ex01.cpp
+++ Cpp_LabSheet/PAGE_02/page02_ex01.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 int main()
 {
-    int ar[] = { 4.5, 5.5, 6.5 };
-    cout << ar[1] / 2;
+    cout<<"Omar Al-mashaqbeh"<<endl;
+    cout<<"2533098"<<endl;
+    cout<<"Computer Science"<<endl;
     return 0;
 }
